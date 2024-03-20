ac9fbe6e1aa9e0c09f83fa584cee3556edeb3512
Sabir Hassan <sabirbk06@gmail.com>
2024-03-20 21:50:28 +0500

    respond with pong message

--- app/main.js
+++ app/main.js
@@ -4,7 +4,7 @@ const net = require('net')
 console.log('Logs from your program will appear here!')
 
 const server = net.createServer((connection) => {
-  // Handle connection
+  connection.write('+PONG\r\n')
 })
 
 server.listen(6379, '127.0.0.1')
