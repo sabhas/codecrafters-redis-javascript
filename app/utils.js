045b5a0fb4ded4e55b6b500cabcd14dd505d9caf
Sabir Hassan <sabirbk06@gmail.com>
2024-03-21 22:35:13 +0500

    Initial Replication ID and Offset

--- app/utils.js
+++ app/utils.js
@@ -19,7 +19,7 @@ const getSysInfo = (parsedArguments) => {
   return {
     role: parsedArguments.hasOwnProperty('replicaof') ? 'slave' : 'master',
     connected_slaves: 0,
-    master_replid: 0,
+    master_replid: '8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb',
     master_repl_offset: 0
   }
 }
