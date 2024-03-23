18491f8bae1134dc7535c57abb39ff28d3eada33
Sabir Hassan <sabirbk06@gmail.com>
2024-03-23 12:12:48 +0500

    Empty RDB Transfer

--- app/main.js
+++ app/main.js
@@ -7,7 +7,7 @@ const {
 } = require('./redisSerializableParser')
 const { setKeyInMap, getKeyFromMap } = require('./memObj')
 const { getSysInfo, getPort, getReplica } = require('./utils')
-const { CRLF } = require('./constants')
+const { CRLF, EMPTY_RDB_FILE_HEX } = require('./constants')
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log('Logs from your program will appear here:')
@@ -58,10 +58,25 @@ const handlers = {
   get: (args) => encodeSingleString(getKeyFromMap(args[0])),
   info: () => encodeBulkString(getRedisInfo()),
   replconf: () => encodeSingleString('OK'),
-  psync: () => {
+  psync: (args, connection) => {
     const sysInfo = getSysInfo(process.argv)
-    return encodeSingleString(
-      `FULLRESYNC ${sysInfo.master_replid} ${sysInfo.master_repl_offset}`
+
+    connection.write(
+      encodeSingleString(
+        `FULLRESYNC ${sysInfo.master_replid} ${sysInfo.master_repl_offset}`
+      )
+    )
+
+    // converts the hexadecimal string of the empty RDB file into a binary buffer
+    const RDB_File_Binary = Buffer.from(EMPTY_RDB_FILE_HEX, 'hex')
+
+    // concatenating a bulk string header (which includes the length of the binary data followed by \r\n) with the binary RDB file content.
+    // The Buffer.concat method is used to merge the header and the file content into a single buffer, which is then written to the connection
+    connection.write(
+      Buffer.concat([
+        Buffer.from(`$${RDB_File_Binary.length}\r\n`),
+        RDB_File_Binary
+      ])
     )
   }
 }
@@ -73,8 +88,8 @@ const server = net.createServer((connection) => {
       const parsedQuery = parseRequest(query)
       const command = parsedQuery[0]
       const args = parsedQuery.slice(1)
-      const resp = handlers[command.toLowerCase()](args)
-      connection.write(resp)
+      const resp = handlers[command.toLowerCase()](args, connection)
+      if (resp) connection.write(resp)
     } catch (e) {
       console.error(e)
       connection.write(e.message + CRLF)
